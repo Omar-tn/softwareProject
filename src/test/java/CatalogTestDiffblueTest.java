28f9fbb9ee5bf2a8316af50aa478a568d194b56a
Omar-tn <omar.omar5750@gmail.com>
2023-12-26 19:01:34 +0200

    coverage commit

---
+++ src/test/java/CatalogTestDiffblueTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+public class CatalogTestDiffblueTest {
+    /**
+     * Method under test: {@link CatalogTest#theProductCatalogIsPopulated()}
+     */
+    @Test
+    public void testTheProductCatalogIsPopulated() {
+        // TODO: Complete this test.
+        //   Reason: R002 Missing observers.
+        //   Diffblue Cover was unable to create an assertion.
+        //   There are no fields that could be asserted on.
+
+        // Arrange and Act
+        (new CatalogTest()).theProductCatalogIsPopulated();
+    }
+}
